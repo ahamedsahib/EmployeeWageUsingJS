5ee89f59b2dcfbcebba59471b4ec25bf3a02dbb5
ahamedsahib <adsahib39@gmail.com>
2021-08-12 18:20:52 +0530

    [Ahamed]Adding while Loop to find total wage of employee in a month and working hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,6 +6,7 @@ const FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const NO_OF_WORKING_DAYS=20;
+const MAX_WORKING_HRS_IN_MONTH = 160;
 
 //generate random value between 0-2(0,1,2) 
 let empCheck=Math.floor(Math.random()*10)%3;
@@ -16,8 +17,9 @@ else
 
 //UC2-UC3
 let empHrs = 0;
-function getWorkingHours(empcheck)
+function getWorkingHours()
 {
+    empCheck=Math.floor(Math.random()*10)%3;
     switch(empCheck)
     {
         case FULL_TIME:
@@ -29,11 +31,19 @@ function getWorkingHours(empcheck)
     }
 }
 //UC4   
-for (let day=0;day<=NO_OF_WORKING_DAYS;day++)
+for (let day=0;day<NO_OF_WORKING_DAYS;day++)
 {
-    empCheck=Math.floor(Math.random()*10)%3;
-    empHrs+= getWorkingHours(empCheck);
+    empHrs+= getWorkingHours();
 }
+//UC5
+let day=0;
+empHrs =0;
+while(day++ <NO_OF_WORKING_DAYS && empHrs<=MAX_WORKING_HRS_IN_MONTH)
+{
+    empHrs += getWorkingHours();
+}
+console.log(`Total days = ${day-1}`);
+console.log(`Total Employee Work hours in a Month = ${empHrs}`);
 //Calling getWorkingHours
 //calculate total wage based on emphrs
 let totalWage =empHrs*WAGE_PER_HR;
